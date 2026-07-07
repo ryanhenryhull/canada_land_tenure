80be3430c5fd70f01e16094a671c13bcc98ea956
Ryan Hull <ryan.h.hull@gmail.com>
2026-07-07 14:38:26 -0400

    preparing for stac map, not functional.

---
+++ scripts/05_create_stac_viewer.py
@@ -0,0 +1,22 @@
+# Ryan Hull
+# Quantitative Biodiversity Lab
+# Goal: from outputs/stac/catalog.json, make a STAC viewer of all our layers.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main():
+    the
+
+if __name__ == "__main__":
+    main()
