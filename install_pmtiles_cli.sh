11634e1296964324317b416c7cb6282cb46bf15a
Ryan Hull <ryan.h.hull@gmail.com>
2026-08-04 11:54:43 -0400

    Full layer info box info for all layers.

--- install_pmtiles_cli.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl -L -o pmtiles.tar.gz https://github.com/protomaps/go-pmtiles/releases/download/v1.30.0/go-pmtiles_1.30.0_Linux_x86_64.tar.gz
-tar -xzf pmtiles.tar.gz
-chmod +x pmtiles
-sudo mv pmtiles /usr/local/bin/
-pmtiles version
