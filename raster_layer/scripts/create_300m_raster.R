51b95eb89622a3d2956b4785b997eae43821a988
Ryan Hull <ryan.h.hull@gmail.com>
2026-09-04 16:21:07 -0400

    finalized r script.

--- raster_layer/scripts/create_300m_raster.R
+++ raster_layer/scripts/create_300m_raster.R
@@ -5,7 +5,9 @@
 
 
 
-# 1. libraries
+
+# 1:
+# Libraries
 rm(list=ls())
 library(sf)
 library(dplyr)
@@ -16,7 +18,8 @@ library(foreign)
 
 
 
-# 2. constants 
+# 2:
+# Constants 
 CRS <- "EPSG:3978"
 RESOLUTION <- 300
 
@@ -30,11 +33,16 @@ rasterize_presence <- function(sf_list, template) {
   Reduce(terra::cover, layer_rasters)
 }
 
+# set session gdal size processing limit to no limit (0) to allow cpcad to process
+Sys.setenv(OGR_GEOJSON_MAX_OBJ_SIZE = "0")
 
 
-# 3. indigenous land data
 
-aboriginal_land_canada <- st_read("data/raw/NA_CA_atlas.gpkg")
+
+# 3:
+# Indigenous land data
+
+aboriginal_land_canada <- st_read("data/processed/canada/aboriginal_lands_canada_legislative_boundaries.geojson")
 
 # land governed by the metis settlements general council as per provincial legislation,
 # not incl. in aboriginal lands database
@@ -47,16 +55,28 @@ mn_first_nation_non_TLE_agreements <- st_read("data/processed/manitoba/manitoba_
 mn_first_nation_permit_fee_simple_lands <- st_read("data/processed/manitoba/manitoba_first_nation_permit_fee_simple_lands.geojson")
 mn_treaty_land_entitlement_sites <- st_read("data/processed/manitoba/manitoba_treaty_land_entitlement_sites.geojson")
 
-indigenous_layers <- list(aboriginal_land_canada,
-                          ab_metis_settlement,
-                          mn_treaty_land_entitlement_sites,
-                          mn_first_nation_permit_fee_simple_lands,
-                          mn_first_nation_non_TLE_agreements,
-                          mn_first_nation_other_TLE_acquisitions)
+indigenous_layers_with_AL_TA <- list(
+  aboriginal_land_canada,
+  ab_metis_settlement,
+  mn_treaty_land_entitlement_sites,
+  mn_first_nation_permit_fee_simple_lands,
+  mn_first_nation_non_TLE_agreements,
+  mn_first_nation_other_TLE_acquisitions
+)
+
+indigenous_layers_no_AL_TA <- list(
+  ab_metis_settlement,
+  mn_treaty_land_entitlement_sites,
+  mn_first_nation_permit_fee_simple_lands,
+  mn_first_nation_non_TLE_agreements,
+  mn_first_nation_other_TLE_acquisitions
+)
 
 
 
-# 4. protected land data
+
+# 4:
+# Protected land data
 
 # canadian database, 2025
 cpcad <- st_read("data/processed/canada/ProtectedConservedArea_2025.geojson")
@@ -75,7 +95,7 @@ qc_protected_zones <- st_read("data/processed/quebec/protected_areas/AP_ZON_S.ge
 
 # both cpcad and below include projected areas, with qc having more. However
 # quebec's layer includes identified critical habitats that are not protected
-# or planned to be protected, which shouldn't be included.
+# or planned to be protected, which shouldn't be included. Don't use for now.
 qc_projected_proteted_areas_and_critical_habitats <- st_read("data/processed/quebec/protected_areas/T_IMP_S.geojson")
 
 # large quantities of sask (private) conservation easements not incl. in cpcad
@@ -86,19 +106,30 @@ sk_crown_conservation_easements <- st_read("data/processed/saskatchewan/saskatch
 # two sources and cpcad, but can be included
 sk_protected_conserved_network <- st_read("data/processed/saskatchewan/saskatchewan_protected_conserved_network.geojson")
 
-protected_layers <- list(cpcad,
-                         NL_Provincial_Protected_Areas,
-                         ns_protected_areas_system,
-                         qc_protected_areas,
-                         qc_protected_zones,
-                         sk_conservation_easements,
-                         sk_crown_conservation_easements,
-                         sk_protected_conserved_network)
+protected_layers_with_cpcad <- list(
+  cpcad,
+  NL_Provincial_Protected_Areas,
+  ns_protected_areas_system,
+  qc_protected_areas,
+  qc_protected_zones,
+  sk_conservation_easements,
+  sk_crown_conservation_easements,
+  sk_protected_conserved_network)
+                                    
+protected_layers_no_cpcad <- list(
+  NL_Provincial_Protected_Areas,
+  ns_protected_areas_system,
+  qc_protected_areas,
+  qc_protected_zones,
+  sk_conservation_easements,
+  sk_crown_conservation_easements,
+  sk_protected_conserved_network)
 
 
 
 
-# 5. 2020 forest based tenure raster data 
+# 5:
+# 2020 forest based tenure raster data 
 
 # Use for water, private land, and other tenure & protection categories.
 canada_forest_management <- rast("data/processed/canada/canada_forest_management_2020/Canada_MFv2020.tif")
@@ -106,65 +137,89 @@ canada_forest_management_vat <- read.dbf("data/processed/canada/canada_forest_ma
 crs(canada_forest_management) # Albers Equal Area 
 res(canada_forest_management) # 250m
 
+# create lookup table for category codes
+# see https://storymaps.arcgis.com/stories/52b71ae81a4e4ccba5771a42c2a32b25
+unique(canada_forest_management_vat$Value)
+forest_management_lookup <- c(
+  water=100,
+  protected=20,
+  treaty=40,
+  indian_reserve=32,
+  federal_reserve=31,
+  restricted=33,
+  private=50,
+  long_term_tenure=11,
+  short_term_tenure=12,
+  other=13
+)
+
 # 300m template grid. Use forest management raster's extent and CRS
 forest_management_reproj <- project(canada_forest_management, CRS, method = "near")
 template  <- rast(ext(forest_management_reproj), resolution = RESOLUTION , crs = CRS)
 forest_management_reproj <- resample(forest_management_reproj, template, method = "near")
-
-# generate codes to create categories later (note to check these in the vat) 
-water_code <- canada_forest_management_vat$MF_code[
-  canada_forest_management_vat$Value == "Water"
-  ]
-
-private_code <- canada_forest_management_vat$MF_code[
-  canada_forest_management_vat$Value == "Private"
-  ]
-
-other_codes <- canada_forest_management_vat$MF_code[
-  canada_forest_management_vat$Value %in% c("Restricted","Federal Reserve")
-  ]
+forest_management_reproj <- subst(forest_management_reproj, 0, NA) # make ocean NA instead of 0, so we can mask at the end
 
 
 
 
+# 6:
+# Create Version 1: Simplified
 
-# 6. Create Version 1: With protected areas, simplified
 # Categories, with priority order:
 #  1. Water
 #  2. Protected Land 
 #  3. Indigenous Land
 #  4. Private Land
 #  5. Other tenure & protection
-#  5. Public Land 
+#  6. Public Land 
 
 # create raster categories
-  CATEGORIES_V1 <- c(
-  water      = 1L,
-  protected  = 2L,
+CATEGORIES_V1 <- c(
+  water = 1L,
+  protected = 2L,
   indigenous = 3L,
-  private    = 4L,
-  other      = 5L,
-  public     = 6L
+  private = 4L,
+  other = 5L,
+  public = 6L
 )
 
-# extract needed categories. Resulting rasters are "binary" of cat code or NA.
-water <- ifel(forest_management_reproj == water_code, CATEGORIES_V1["water"], NA) # terra's raster ifelse fn
-private <- ifel(forest_management_reproj == private_code, CATEGORIES_V1["private"], NA) 
-other <- ifel(forest_management_reproj %in% other_codes, CATEGORIES_V1["other"], NA)
 
-# public: set as all of canada within template. set as the default category.
-# init(x, fun) will make a raster with extent and crs of x, and fill it based on fun
-public <- init(template, unname(CATEGORIES_V1["public"])) 
+# colors by category name 
+tenure_colors <- c(
+  water      = "#4575B4",
+  protected  = "#2A9D5B",
+  indigenous = "#D98C3F",
+  private    = "#8C8C8C",
+  other      = "#8E5A9E",
+  public     = "#D8C58A"
+)
+
+# extract needed categories from forest management layer as separate layers.
+# Resulting rasters are "binary" of cat code or NA, using terra's raster ifelse fn
+water   <- ifel(forest_management_reproj == forest_management_lookup["water"],
+                CATEGORIES_V1["water"],
+                NA) 
+
+private <- ifel(forest_management_reproj == forest_management_lookup["private"],
+                CATEGORIES_V1["private"],
+                NA) 
+
+other   <- ifel(forest_management_reproj %in% c(forest_management_lookup["federal_reserve"], forest_management_lookup["restricted"]),
+                CATEGORIES_V1["other"],
+                NA)
+
+# give public value to all cells that are not NA in the forest management raster
+public <- ifel(!is.na(forest_management_reproj), CATEGORIES_V1["public"], NA)
 
 # transform vectors 
 # rasterize gives 1 when polygon covers cell, NA otherwise.
 # then multiply by category code.
-protected  <- rasterize_presence(protected_layers, template)  * CATEGORIES_V1["protected"] 
-indigenous <- rasterize_presence(indigenous_layers, template) * CATEGORIES_V1["indigenous"]
+protected  <- rasterize_presence(protected_layers_with_cpcad, template)  * CATEGORIES_V1["protected"] 
+indigenous <- rasterize_presence(indigenous_layers_with_AL_TA, template) * CATEGORIES_V1["indigenous"]
 
 # create raster
 # reduce applies cover pairwise to ensure the ordering
-tenure_300m <- Reduce(terra::cover, list(
+tenure_300m_v1 <- Reduce(terra::cover, list(
   water,
   protected,
   indigenous,
@@ -173,31 +228,110 @@ tenure_300m <- Reduce(terra::cover, list(
   public # catches all remaining, ie no NAs
 ))
 
+# Mask the layer to exclude ocean
+tenure_300m_v1 <- mask(tenure_300m_v1, forest_management_reproj)
+
 # link up the metadata of category names 
-levels(tenure_300m) <- data.frame(
+levels(tenure_300m_v1) <- data.frame(
   value = unname(CATEGORIES_V1),
-  category = names(CATEGORIES_V1)
+  category = tools::toTitleCase(names(CATEGORIES_V1)) # capitalised
 )
 
-writeRaster(tenure_300m, "raster_layer/outputs/canada_tenure_v1_300m.tif",
-            overwrite = TRUE, datatype = "INT1U") # INT1U for 0-255, ie to keep this light vs standard
+# attach coloring to file
+coltab(tenure_300m_v1) <- data.frame(
+  value = unname(CATEGORIES_V1),
+  col   = tenure_colors[names(CATEGORIES_V1)]
+)
+
+# write out as geotiff
+writeRaster(tenure_300m_v1, "raster_layer/outputs/ten_layers_land_tenure_protection_v1.tif",
+            overwrite = TRUE, datatype = "INT1U") # INT1U for 0-255, ie to  keep this light
 
 
 
 
-# 7. Create Version 2 : With protected areas, more precise
+
+# 7:
+# Create Version 2 : more precise
+
 # Categories, with priority order:
 #  1. Water
-#  2. Indigenous land: Aboriginal lands of Canada Legislative Boundaries
-#  3. Indigenous land: Other
-#  4. Protected areas: CPCAD
-#  5. Protected areas: Other
+#  2. Protected areas: CPCAD
+#  3. Protected areas: Other
+#  4. Indigenous land: Aboriginal lands of Canada Legislative Boundaries
+#  5. Indigenous land: Other
 #  6. Private Land
-#  7. Public Land 
+#  7. Other tenure & protection
+#  8. Public Land 
+
+CATEGORIES_V2 <- c(
+  water = 1L,
+  protected_cpcad  = 2L,
+  protected_other = 3L,
+  indigenous_legislative_boundaries = 4L,
+  indigenous_other = 5L,
+  private = 6L,
+  other = 7L,
+  public = 8L
+)
+
+tenure_colors_v2 <- c(
+  water            = "#4575B4",
+  protected_cpcad  = "#2A9D5B",
+  protected_other  = "#8BCB8A",
+  indigenous_legislative_boundaries = "#D98C3F",
+  indigenous_other = "#E8B978",
+  private          = "#8C8C8C",
+  other            = "#8E5A9E",
+  public           = "#D8C58A"
+)
+
+water   <- ifel(forest_management_reproj == forest_management_lookup["water"],
+                CATEGORIES_V2["water"],
+                NA) 
+
+private <- ifel(forest_management_reproj == forest_management_lookup["private"],
+                CATEGORIES_V2["private"],
+                NA) 
+
+other   <- ifel(forest_management_reproj %in% c(forest_management_lookup["federal_reserve"], forest_management_lookup["restricted"]),
+                CATEGORIES_V2["other"],
+                NA)
+
+public <- ifel(!is.na(forest_management_reproj), CATEGORIES_V2["public"], NA)
+
+protected_cpcad  <- rasterize_presence(list(cpcad), template)  * CATEGORIES_V2["protected_cpcad"] 
+protected_other <- rasterize_presence(protected_layers_no_cpcad, template) * CATEGORIES_V2["protected_other"]
+indigenous_legislative_boundaries <- rasterize_presence(list(aboriginal_land_canada), template) * CATEGORIES_V2["indigenous_legislative_boundaries"]
+indigenous_other <- rasterize_presence(indigenous_layers_no_AL_TA, template) * CATEGORIES_V2["indigenous_other"]
+
+tenure_300m_v2 <- Reduce(terra::cover, list(
+  water,
+  protected_cpcad,
+  protected_other,
+  indigenous_legislative_boundaries,
+  indigenous_other,
+  private,
+  other,
+  public 
+))
+
+tenure_300m_v2 <- mask(tenure_300m_v2, forest_management_reproj)
+
+levels(tenure_300m_v2) <- data.frame(
+  value = unname(CATEGORIES_V2),
+  category = tools::toTitleCase(names(CATEGORIES_V2))
+)
+
+coltab(tenure_300m_v2) <- data.frame(
+  value = unname(CATEGORIES_V2),
+  col   = tenure_colors_v2[names(CATEGORIES_V2)]
+)
+
+writeRaster(tenure_300m_v2, "raster_layer/outputs/ten_layers_land_tenure_projection_v2.tif",
+            overwrite = TRUE, datatype = "INT1U") 
 
 
 
-
-# 8. Create Version 3 : With protected areas, even more precise
-
-
+plot(tenure_300m_v1)
+plot(tenure_300m_v2)
--- scripts/02_make_cogs.py
+++ scripts/02_make_cogs.py
@@ -15,6 +15,7 @@ from rio_cogeo.profiles import cog_profiles
 SCRIPTS_DIR = Path(__file__).parent
 PROJECT_ROOT = SCRIPTS_DIR.parent
 PROCESSED_DIR = PROJECT_ROOT/"data"/"processed" # this already contains only tifs/tiffs.
+RASTER_OUTPUTS_DIR = PROJECT_ROOT/"raster_layer"/"outputs"
 OUT_DIR = PROJECT_ROOT/"outputs"/"cogs"
 RASTER_EXTENSIONS = (".tiff", ".tif")
 RESAMPLING = "nearest"
@@ -45,13 +46,13 @@ def make_cog(src_path: Path, dst_path: Path):
 # Perform transformation. Note currently this does not place the files in their dedicated country or province folder.
 """ Loop through all processed tif/tiffs to create the COGs """
 def main():
-    sources = [file for file in PROCESSED_DIR.glob("**/*") if file.suffix.lower() in RASTER_EXTENSIONS]
+    # note swap RASTER_OUTPUTS_DIR and PROCESSED_DIR depending on source.
+    sources = [file for file in RASTER_OUTPUTS_DIR.glob("**/*") if file.suffix.lower() in RASTER_EXTENSIONS]
     if not sources:
         print("no tifs/tiffs found")
         return
     for source in sources:
         make_cog(source, OUT_DIR / f"{source.stem}.tif")
-
 
 
 if __name__ == "__main__":
